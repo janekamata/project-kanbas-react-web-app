ee700a819ea8b265ef1f8e9d8f1102e7e4b8474f
Jane Kamata <janekamata8@gmail.com>
2024-09-16 21:55:45 -0400

    Completed up to 3.4 - start at https://docs.google.com/document/d/1O_Vyk-9fNEBVYoEH_oFrhDcDTVJufhmjv3NzXJqwo44/edit#heading=h.z8fze1g8lgum

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Labs from "./Labs";
+import Kanbas from "./Kanbas";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
@@ -9,8 +10,9 @@ function App() {
     <HashRouter>
       <div>
         <Routes>
-          <Route path="/" element=<Navigate to="Labs" /> />
+          <Route path="/" element=<Navigate to="Kanbas" /> />
           <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
         </Routes>
       </div>
     </HashRouter>
