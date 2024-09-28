ff70bcd0499c4670ca776caccc714c7330a5ff3a
Jane Kamata <janekamata8@gmail.com>
2024-09-27 22:19:15 -0400

    Working on lab; up to https://docs.google.com/document/d/1vmoLm12fKv7W3y3H-2HdsPzy4GvNrdyTfic6BLoE5yA/edit#heading=h.5eyvwed3sc50

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,4 +1,17 @@
 import "./index.css";
+import ForegroundColors from "./ForegroundColors";
+import BackgroundColors from "./BackgroundColors";
+import Borders from "./Borders";
+import Padding from "./Padding";
+import Margins from "./Margins";
+import Corners from "./Corners";
+import Dimensions from "./Dimensions";
+import Positions from "./Positions";
+import Zindex from "./Zindex";
+import Float from "./Float";
+import GridLayout from "./GridLayout";
+import Flex from "./Flex";
+import ReactIconsSampler from "./ReactIcons";
 
 export default function Lab2() {
     return (
@@ -31,7 +44,39 @@ export default function Lab2() {
                     This heading has same style as paragraph above
                 </h4>
             </div>
-
+            <div id="wd-css-document-structure">
+                <div className="wd-selector-1">
+                    <h3>Document structure selectors</h3>
+                    <div className="wd-selector-2">
+                        Selectors can be combined to refer elements in particular
+                        places in the document
+                        <p className="wd-selector-3">
+                            This paragraph's red background is referenced as
+                            <br />
+                            .selector-2 .selector3<br />
+                            meaning the descendant of some ancestor.<br />
+                            <span className="wd-selector-4">
+                                Whereas this span is a direct child of its parent
+                            </span><br />
+                            You can combine these relationships to create specific
+                            styles depending on the document structure
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <ForegroundColors />
+            <BackgroundColors />
+            <Borders />
+            <Padding />
+            <Margins />
+            <Corners />
+            <Dimensions />
+            <Positions />
+            <Zindex />
+            <Float />
+            <GridLayout />
+            <Flex />
+            <ReactIconsSampler />
         </div>
 
     );
