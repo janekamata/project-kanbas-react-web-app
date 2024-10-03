5ce02127651eb196c08d67de05ec1690c7c4c4c4
Jane Kamata <janekamata8@gmail.com>
2024-10-03 00:31:01 -0400

    Removing breaks from nav

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 export default function AccountNavigation() {
     return (
         <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0 me-2">
-            <Link to={ `/Kanbas/Account/Signin` } className="list-group-item active border border-0 fs-5"> Signin  </Link> <br />
-            <Link to={ `/Kanbas/Account/Signup` } className="list-group-item text-danger border border-0 fs-5"> Signup  </Link> <br />
-            <Link to={ `/Kanbas/Account/Profile` } className="list-group-item text-danger border border-0 fs-5"> Profile </Link> <br />
+            <Link to={ `/Kanbas/Account/Signin` } className="list-group-item active border border-0 fs-5"> Signin  </Link>
+            <Link to={ `/Kanbas/Account/Signup` } className="list-group-item text-danger border border-0 fs-5"> Signup  </Link>
+            <Link to={ `/Kanbas/Account/Profile` } className="list-group-item text-danger border border-0 fs-5"> Profile </Link>
         </div>
     );
 }
