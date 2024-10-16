398c862a70009588d0748c66fc549508f84df506
Jane Kamata <janekamata8@gmail.com>
2024-10-15 22:00:35 -0400

    Completed 3.9

--- src/Kanbas/Database/index.ts
+++ src/Kanbas/Database/index.ts
@@ -1,4 +1,6 @@
 import courses from "./courses.json";
 import modules from "./modules.json";
-import assignments from "./assignments.json"
-export {  courses, modules, assignments  };
+import assignments from "./assignments.json";
+import enrollments from "./enrolllments.json";
+import users from "./users.json";
+export {  courses, modules, assignments , enrollments, users };
