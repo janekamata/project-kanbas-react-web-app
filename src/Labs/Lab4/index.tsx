c4491a3df32cf827461c0f4b35f2960c1eeb506f
Jane Kamata <janekamata8@gmail.com>
2024-10-22 13:22:15 -0400

    Completed lab 2.3.1-2.3.5

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,7 +1,11 @@
+import BooleanStateVariables from "./BooleanStateVariables";
 import ClickEvent from "./ClickEvent";
+import Counter from "./Counter";
+import DateStateVariable from "./DateStateVariable";
 import EventObject from "./EventObject";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
+import StringStateVariables from "./StringStateVariables";
 
 export default function Lab1() {
 function sayHello() {
@@ -14,6 +18,10 @@ function sayHello() {
             <PassingDataOnEvent />
             <PassingFunctions theFunction={sayHello} />
             <EventObject />
+            <Counter />
+            <BooleanStateVariables/>
+            <StringStateVariables/>
+            <DateStateVariable />
         </div>
     );
 }
