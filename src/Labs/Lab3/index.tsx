ee71f87d710020df6a61a991dcd4176f710973ec
Jane Kamata <janekamata8@gmail.com>
2024-10-22 22:51:07 -0400

    Completed lab 2.5 although have question about 2.3.7 list group border color and 2.5.2 input not updating

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -32,49 +32,64 @@ import TodoItem from "./TodoItem";
 import TodoList from "./TodoList";
 import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
+import { useSelector } from "react-redux";
 
 export default function Lab3() {
-    console.log( 'Hello World!' );
-    return (
-        <div>
-            <h2>Lab 3</h2>
-            <VariablesAndConstants />
-            <VariableTypes />
-            <BooleanVariables />
-            <IfElse />
-            <TernaryOperator />
-            <ConditionalOutputIfElse />
-            <ConditionalOutputInline /><hr />
-            <LegacyFunctions />
-            <ArrowFunctions />
-            <ImpliedReturn />
-            <TemplateLiterals />
-            <SimpleArrays />
-            <ArrayIndexAndLength />
-            <AddingAndRemovingToFromArrays />
-            <ForLoops />
-            <MapFunction />
-            <FindFunction />
-            <FindIndex />
-            <FilterFunction />
-            <JsonStringify />
-            <House />
-            <TodoItem />
-            <TodoList />
-            <Spreading />
-            <Destructing />
-            <FunctionDestructing />
-            <DestructingImports />
-            <Classes />
-            <Styles />
-            <Add a={ 3 } b={ 4 } />
-            <h4>Square of 4</h4>
-            <Square>4</Square><hr />
-            <Highlight>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
-                vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
-            </Highlight><hr />
-            <PathParameters />
-        </div>
-    );
+  const { todos } = useSelector((state: any) => state.todosReducer);
+  console.log("Hello World!");
+  return (
+    <div>
+      <h2>Lab 3</h2>
+      <ul className="list-group">
+        {todos.map((todo: any) => (
+          <li className="list-group-item" key={todo.id}>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+      <hr />
+      <VariablesAndConstants />
+      <VariableTypes />
+      <BooleanVariables />
+      <IfElse />
+      <TernaryOperator />
+      <ConditionalOutputIfElse />
+      <ConditionalOutputInline />
+      <hr />
+      <LegacyFunctions />
+      <ArrowFunctions />
+      <ImpliedReturn />
+      <TemplateLiterals />
+      <SimpleArrays />
+      <ArrayIndexAndLength />
+      <AddingAndRemovingToFromArrays />
+      <ForLoops />
+      <MapFunction />
+      <FindFunction />
+      <FindIndex />
+      <FilterFunction />
+      <JsonStringify />
+      <House />
+      <TodoItem />
+      <TodoList />
+      <Spreading />
+      <Destructing />
+      <FunctionDestructing />
+      <DestructingImports />
+      <Classes />
+      <Styles />
+      <Add a={3} b={4} />
+      <h4>Square of 4</h4>
+      <Square>4</Square>
+      <hr />
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione
+        eaque illo minus cum, saepe totam vel nihil repellat nemo explicabo
+        excepturi consectetur. Modi omnis minus sequi maiores, provident
+        voluptates.
+      </Highlight>
+      <hr />
+      <PathParameters />
+    </div>
+  );
 }
