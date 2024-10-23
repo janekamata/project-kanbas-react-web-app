6d9c92214982dc04be6e1dc1954d6f05bacc3b8a
Jane Kamata <janekamata8@gmail.com>
2024-10-22 21:55:16 -0400

    Completed lab 2.4

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,6 +1,5 @@
 import ArrayStateVariable from "./ArrayStateVariable";
 import BooleanStateVariables from "./BooleanStateVariables";
-import ChildStateComponent from "./ChildStateComponent";
 import ClickEvent from "./ClickEvent";
 import Counter from "./Counter";
 import DateStateVariable from "./DateStateVariable";
@@ -9,26 +8,28 @@ import ObjectStateVariable from "./ObjectStateVariable";
 import ParentStateComponent from "./ParentStateComponent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
+import ReduxExamples from "./ReduxExamples";
 import StringStateVariables from "./StringStateVariables";
 
 export default function Lab1() {
-function sayHello() {
+  function sayHello() {
     alert("Hello");
-}
+  }
 
-    return (
-        <div>
-            <ClickEvent />
-            <PassingDataOnEvent />
-            <PassingFunctions theFunction={sayHello} />
-            <EventObject />
-            <Counter />
-            <BooleanStateVariables/>
-            <StringStateVariables/>
-            <DateStateVariable />
-            <ObjectStateVariable />
-            <ArrayStateVariable />
-            <ParentStateComponent />
-        </div>
-    );
+  return (
+    <div>
+      <ClickEvent />
+      <PassingDataOnEvent />
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject />
+      <Counter />
+      <BooleanStateVariables />
+      <StringStateVariables />
+      <DateStateVariable />
+      <ObjectStateVariable />
+      <ArrayStateVariable />
+      <ParentStateComponent />
+      <ReduxExamples />
+    </div>
+  );
 }
