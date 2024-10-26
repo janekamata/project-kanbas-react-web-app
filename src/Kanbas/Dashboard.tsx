e357e41a61ce5435aac6e212914f3fe941aa6569
Jane Kamata <janekamata8@gmail.com>
2024-10-26 17:27:58 -0400

    Completed 3.2

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -1,39 +1,21 @@
 import { Link } from "react-router-dom";
 import React, { useState } from "react";
 import * as db from "./Database";
-export default function Dashboard() {
-  const [courses, setCourses] = useState<any[]>(db.courses);
-  const initialCourse = {
-    _id: "0",
-    name: "New Course",
-    number: "New Number",
-    startDate: "2023-09-10",
-    endDate: "2023-12-15",
-    image: "/images/reactjs.jpg",
-    description: "New Description",
-  };
-  const [course, setCourse] = useState<any>(initialCourse);
-
-  const addNewCourse = () => {
-    const newCourse = { ...course, _id: new Date().getTime().toString() };
-    setCourses([...courses, { ...course, ...newCourse }]);
-  };
-  const deleteCourse = (courseId: string) => {
-    setCourses(courses.filter((course) => course._id !== courseId));
-  };
-  const updateCourse = () => {
-    setCourses(
-      courses.map((c) => {
-        if (c._id === course._id) {
-          return course;
-        } else {
-          return c;
-        }
-      })
-    );
-    setCourse(initialCourse);
-  };
-
+export default function Dashboard({
+  courses,
+  course,
+  setCourse,
+  addNewCourse,
+  deleteCourse,
+  updateCourse,
+}: {
+  courses: any[];
+  course: any;
+  setCourse: (course: any) => void;
+  addNewCourse: () => void;
+  deleteCourse: (course: any) => void;
+  updateCourse: () => void;
+}) {
   const images = [
     { label: 1, path: "web" },
     { label: 2, path: "coop" },
