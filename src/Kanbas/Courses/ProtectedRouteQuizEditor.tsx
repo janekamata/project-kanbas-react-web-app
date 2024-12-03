0be5c7fa17a9dabcafe2a85871a9f421c52ce7c8
Jane Kamata <janekamata8@gmail.com>
2024-12-03 00:43:09 -0500

    Adding database back for testing

--- src/Kanbas/Courses/ProtectedRouteQuizEditor.tsx
+++ src/Kanbas/Courses/ProtectedRouteQuizEditor.tsx
@@ -1,3 +1,4 @@
+import { current } from "@reduxjs/toolkit";
 import { useSelector } from "react-redux";
 import { Navigate, useParams } from "react-router-dom";
 export default function ProtectedRouteQuizEditor({
@@ -8,7 +9,7 @@ export default function ProtectedRouteQuizEditor({
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const { cid } = useParams();
 
-  if (currentUser.role === "FACULTY") {
+  if (currentUser.role === "FACULTY" || currentUser.role === "ADMIN") {
     return children;
   } else {
     return <Navigate to={`/Kanbas/Courses/${cid}/Quizzes`} />;
